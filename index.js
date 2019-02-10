b5b4510841db34ba4f8c97ece04ead949101ce57
Johannes Loher <johannes.loher@maibornwolff.de>
2019-02-10 18:52:34 +0100

    feat: run all scenarios provided as commandline parameters

--- index.js
+++ index.js
@@ -3,18 +3,25 @@ const FS = require('fs');
 
 const RESOURCES = process.env.ALT_SRC;
 
-const findFileNameForId = (scenarioId) => {
-    return FS.readdirSync(RESOURCES + '/scenarios').find(scenario => scenario.startsWith(scenarioId + "-"));
-}
+const findFileNameForId = scenarioId => {
+  return FS.readdirSync(RESOURCES + '/scenarios').find(scenario =>
+    scenario.startsWith(scenarioId + '-')
+  );
+};
 
-let scenarioPath = process.argv[2].toString().endsWith('yaml') ? 
-                    `${RESOURCES}/scenarios/${process.argv[2]}` 
-                    :
-                    `${RESOURCES}/scenarios/${findFileNameForId(process.argv[2])}`;
+const getScenarioPath = scenario =>
+  scenario.toString().endsWith('yaml')
+    ? `${RESOURCES}/scenarios/${scenario}`
+    : `${RESOURCES}/scenarios/${findFileNameForId(scenario)}`;
 
-ALT.runScenario(
-    scenarioPath, 
-    RESOURCES + '/actions', 
-    process.env.OUT_SRC, 
-    RESOURCES + '/environment/config.yaml'
-);
+process.argv
+  .slice(2)
+  .map(getScenarioPath)
+  .forEach(scenarioPath =>
+    ALT.runScenario(
+      scenarioPath,
+      RESOURCES + '/actions',
+      process.env.OUT_SRC,
+      RESOURCES + '/environment/config.yaml'
+    )
+  );
